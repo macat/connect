b01c613b500a1d088e4907f5038a40e29680458f
George Brocklehurst <george@georgebrock.com>
2014-11-13 18:11:56 -0500

    Display results on their own page.
    Now that we show individual import results for each candidate, a flash
    message doesn't really cut it: we need to be able to display multiple lines.

--- app/controllers/jobvite_imports_controller.rb
+++ app/controllers/jobvite_imports_controller.rb
@@ -1,7 +1,6 @@
 class JobviteImportsController < ApplicationController
   def create
     jobvite_import = Jobvite::Import.new(current_user)
-    status = jobvite_import.import
-    redirect_to dashboard_path, notice: status
+    @status = jobvite_import.import
   end
 end
