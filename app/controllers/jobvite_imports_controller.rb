ad517e653d6a58b241b5e4fa9a5bc0f7a4f16575
Rafael George <george.rafael@gmail.com>
2014-12-05 17:09:02 -0400

    Add proper change for displaying list of imported candidates

--- app/controllers/jobvite_imports_controller.rb
+++ app/controllers/jobvite_imports_controller.rb
@@ -1,6 +1,6 @@
 class JobviteImportsController < ApplicationController
   def create
     jobvite_import = Jobvite::Import.new(current_user)
-    @status = jobvite_import.import
+    @candidates = jobvite_import.import.to_a
   end
 end
