a3e58f4b121deb98383d345ef190c1a4a1e0fd63
Attila Maczak <attila@maczak.hu>
2014-12-20 20:09:40 -0500

    use webmock for mocking api responses

--- spec/features/user_visits_dashboard_spec.rb
+++ spec/features/user_visits_dashboard_spec.rb
@@ -1,16 +1,29 @@
 require "rails_helper"
 
 feature "User visits their dashboard" do
-  scenario "with a Jobvite connection, but no Jobvite field on Namely" do
-    user = create(:user)
-    create(
-      :jobvite_connection,
-      :connected,
-      user: user,
-      found_namely_field: false,
-    )
+  let(:api_host) do
+    "%{protocol}://%{subdomain}.namely.com" % {
+      protocol: Rails.configuration.namely_api_protocol,
+      subdomain: ENV['TEST_NAMELY_SUBDOMAIN'],
+    }
+  end
+  let(:fixture_file) { "fields_without_jobvite" }
+  before do
+    stub_request(:get, "#{ api_host }/api/v1/profiles/fields")
+      .with(query: {access_token: ENV['TEST_NAMELY_ACCESS_TOKEN'] })
+      .to_return(status: 200, body: File.read("spec/fixtures/api_responses/#{ fixture_file }.json"))
+  end
+  context "with a Jobvite connection, but no Jobvite field on Namely" do
+    scenario do
+      user = create(:user)
+      create(
+        :jobvite_connection,
+        :connected,
+        user: user,
+        found_namely_field: false,
+      )
 
-    VCR.use_cassette("namely_fields_without_jobvite_id") do
+
       visit dashboard_path(as: user)
 
       expect(page).to have_content t(
@@ -20,20 +33,23 @@ feature "User visits their dashboard" do
     end
   end
 
-  scenario "with a Jobvite connection, and a Jobvite field on Namely" do
-    user = create(:user)
-    create(
-      :jobvite_connection,
-      :connected,
-      user: user,
-      found_namely_field: true,
-    )
+  context "with a Jobvite connection, and a Jobvite field on Namely" do
+    let(:fixture_file) { "fields_with_jobvite" }
+    scenario do
+      user = create(:user)
+      create(
+        :jobvite_connection,
+        :connected,
+        user: user,
+        found_namely_field: true,
+      )
 
-    visit dashboard_path(as: user)
+      visit dashboard_path(as: user)
 
-    expect(page).not_to have_content t(
-      "dashboards.show.missing_namely_field",
-      name: "jobvite_id",
-    )
+      expect(page).not_to have_content t(
+        "dashboards.show.missing_namely_field",
+        name: "jobvite_id",
+      )
+    end
   end
 end
