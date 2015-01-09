3a81540bc12202455c28c5f5130684d6a56fa4b0
Rafael George <george.rafael@gmail.com>
2015-01-09 15:37:51 -0500

    Add UserPresenter#full_name

---
+++ spec/presenters/user_presenter_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper' 
+
+describe UserPresenter do 
+  subject(:presenter) { described_class.new(user) }
+
+  describe "#full_name" do
+    let(:user) { double :user, first_name: "Kate", last_name: "Libby" }
+
+    it "combines the first and last names" do
+      expect(presenter.full_name).to eq "Kate Libby"
+    end
+
+    context "when only first name is set" do
+      let(:user) { double :user, first_name: "Kate", last_name: nil }
+
+      it "returns that name" do
+        expect(presenter.full_name).to eq "Kate"
+      end
+    end
+
+    context "when only last name is set" do 
+      let(:user) { double :user, first_name: nil, last_name: "Libby" }
+      it "returns last name" do 
+        expect(presenter.full_name).to eq "Libby"
+      end
+    end
+
+    context "when no names are set" do
+      let(:user) { double :user, first_name: nil, last_name: nil }
+      it "returns the empty string" do
+        expect(presenter.full_name).to eq ""
+      end
+    end
+  end
+end
