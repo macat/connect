e4498a094347a5cf65397114a76a965b18e17dde
Rafael George <george.rafael@gmail.com>
2015-01-16 14:34:49 -0500

    Add new Connect::Users::AccessTokenFreshner to remove refreshing token logic from User model

--- app/connect/users/access_token_freshner.rb
+++ app/connect/users/access_token_freshner.rb
@@ -1,12 +1,38 @@
 module Connect 
   module Users 
     class AccessTokenFreshner 
+      def self.fresh_access_token(user) 
+        new(user).fresh_access_token
+      end
 
       def initialize(user) 
-      end
+        @user = user 
+      end 
 
       def fresh_access_token
+        if access_token_expired?
+          refresh_access_token
+          user.save_token_info(tokens.fetch("access_token"), 
+                               tokens.fetch("expires_in"))
+        end
+        user.access_token
       end
+
+      private 
+
+      def refresh_access_token 
+        @tokens = authenticator.refresh_access_token(user.refresh_token)
+      end
+
+      def access_token_expired?
+        Time.current > user.access_token_expiry
+      end
+
+      def authenticator
+        Authenticator.new(user.subdomain)
+      end
+
+      attr_reader :user, :tokens
     end
   end
 end
