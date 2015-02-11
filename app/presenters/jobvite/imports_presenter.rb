430da968886e649bb1723233a61f0cc91782b85e
cored <george.rafael@gmail.com>
2015-02-11 16:16:03 -0400

    Add a new Jobvite::ImportsPresenter to handle view logic regarding imported and not imported candidates

--- app/presenters/jobvite/imports_presenter.rb
+++ app/presenters/jobvite/imports_presenter.rb
@@ -6,11 +6,22 @@ module Jobvite
 
     def imported_candidates
       import_results.to_a.map do |import_result|
-        import_result[:candidate] if import_result[:result].empty? 
+        import_result[:candidate] if import_result[:result] == I18n.t("status.success")
+      end.compact
+    end
+
+    def not_imported_candidates
+      import_results.to_a.map do |import_result| 
+        format_error_message(import_result) if import_result[:result] =~ /error/i
       end.compact
     end
 
     private 
+
+    def format_error_message(import_result)
+      import_result[:result] = import_result[:result].split(":")[-1]
+      import_result
+    end
 
     attr_reader :import_results
   end
