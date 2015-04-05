5109d339bbdd018dc061b5238ee18fa957bab720
cored <george.rafael@gmail.com>
2015-04-04 21:09:29 -0400

    Remove wisper gem Remove the publisher ConnectionUpdater service Start using lambdas for callbacks for sucess and failure calls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,6 @@ require 'delegate'
 require 'timecop'
 require 'active_support/time'
 
-require 'wisper' 
-require 'wisper/rspec/matchers'
-
 require_relative '../app/services/users/user_with_full_name'
 require_relative '../app/services/users/access_token_freshener'
 require_relative '../app/services/users/token_expiry'
@@ -15,7 +12,6 @@ require_relative '../app/services/jobvite/connection_updater'
 
 # http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  config.include Wisper::RSpec::BroadcastMatcher
   config.expect_with :rspec do |expectations|
     expectations.syntax = :expect
   end
