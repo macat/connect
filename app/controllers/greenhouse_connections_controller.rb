fa1b2494fc67eae752fe0a72f030397f23f84106
cored <george.rafael@gmail.com>
2015-04-28 12:37:35 -0400

    Add #update action and start using the Connection updater because of new name webhook field

--- app/controllers/greenhouse_connections_controller.rb
+++ app/controllers/greenhouse_connections_controller.rb
@@ -1,5 +1,22 @@
 class GreenhouseConnectionsController < ApplicationController
   def edit 
-    @greenhouse_connection = current_user.greenhouse_connection
+    greenhouse_connection
+  end
+
+  def update
+    ConnectionUpdater.new(greenhouse_connection_params, greenhouse_connection).update
+    redirect_to dashboard_path
+  rescue ConnectionUpdater::UpdateFailed
+    render :edit
+  end
+
+  private 
+
+  def greenhouse_connection_params
+    params.require(:greenhouse_connection).permit(:name)
+  end
+
+  def greenhouse_connection
+    @greenhouse_connection ||= current_user.greenhouse_connection
   end
 end
