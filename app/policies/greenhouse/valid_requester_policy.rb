5e07356032a08e0261e5a2d4f1f081fe99b3ed41
cored <george.rafael@gmail.com>
2015-04-29 14:19:11 -0400

    Add proper implementation for checking for HMAC digest for signature

--- app/policies/greenhouse/valid_requester_policy.rb
+++ app/policies/greenhouse/valid_requester_policy.rb
@@ -1,20 +1,30 @@
 module Greenhouse 
   class ValidRequesterPolicy
-    def initialize(connection, signature) 
+    def initialize(connection, signature, body) 
       @connection = connection
       @signature_algorithm, @signature = signature.split(/\s/)
+      @body = body
     end
 
     def valid?
-      
+      build_signature.include? signature 
     end
 
     private 
 
     def digest
-      @digest ||= OpenSSL::Digest.new(signature_algorithm)
+      @digest ||= OpenSSL::Digest.new(signature_algorithm.upcase)
     end
 
-    attr_reader :connection, :signature_algorithm, :signature
+    def build_signature
+      @build_signature = OpenSSL::HMAC.digest(digest, connection.secret_key, 
+                                                   payload).unpack('H*')
+    end
+
+    def payload
+      body.fetch('payload').to_s
+    end
+
+    attr_reader :connection, :signature_algorithm, :signature, :body
   end
 end
--- spec/policies/greenhouse/valid_requester_policy_spec.rb
+++ spec/policies/greenhouse/valid_requester_policy_spec.rb
@@ -1,12 +1,17 @@
 require_relative '../../../app/policies/greenhouse/valid_requester_policy'
 
 describe Greenhouse::ValidRequesterPolicy do 
-  subject(:policy) { described_class.new(connection, signature).valid? }
-  let(:connection) { double :connection, secret_key: 'mysignature' }
-  
+  subject(:policy) { described_class.new(connection, signature, payload).valid? }
+  let(:connection) { double :connection, 
+                     secret_key: 'mysignature' }
+  let(:payload) do 
+    JSON.parse(
+        File.read("spec/fixtures/api_requests/greenhouse_payload_ping.json"))
+  end
+
   describe '#valid?' do 
-    context 'for valid digest' do 
-      let(:signature) { 'sha256 mysignature' }
+    context 'for valid digest in the high nibble' do 
+      let(:signature) { 'sha256 7c051a394b3de31bd493403ca07b96a1e99518321724a882ade6d03a24e0f396' }
       it { is_expected.to be_truthy }
     end
   end
