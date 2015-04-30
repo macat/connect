48970def80754ce8b863e9dc5bda507d74dd90ee
cored <george.rafael@gmail.com>
2015-04-30 16:06:27 -0400

    Move mailer logic into Icims::CandidateImporter

--- app/controllers/icims_candidate_imports_controller.rb
+++ app/controllers/icims_candidate_imports_controller.rb
@@ -3,20 +3,9 @@ class IcimsCandidateImportsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    service = Icims::CandidateImporter.new(Icims::Connection, params)
-    service.import
-    mailer.delay.successful_import(service.user, service.candidate)
-  rescue Icims::CandidateImporter::FailedImport
-    mailer.delay.unsuccessful_import(service.user, 
-                                     service.candidate, 
-                                     service.imported_result)
-  rescue Icims::Client::Error => e
-    mailer.delay.unauthorized_import(service.user, e.message)
-  ensure 
+    Icims::CandidateImporter.new(Icims::Connection, 
+                                 IcimsCandidateImportMailer, 
+                                 params).import
     render text: nil
-  end
-
-  def mailer
-    IcimsCandidateImportMailer
   end
 end
