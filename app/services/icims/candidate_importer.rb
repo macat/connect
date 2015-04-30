d8922871b6b000e8f896b7c2ef93790e83d43543
cored <george.rafael@gmail.com>
2015-04-30 16:06:49 -0400

    Remove params value object not needed Push params logic into #person_id, #customer_id and #api_key Remove FailedImport exception the errors are handle local to the service now

--- app/services/icims/candidate_importer.rb
+++ app/services/icims/candidate_importer.rb
@@ -1,43 +1,47 @@
-module Icims 
+module Icims
   class CandidateImporter
     attr_reader :candidate, :imported_result, :params
 
-    class FailedImport < StandardError; end
-    Params = Struct.new(:params) do 
-      def customer_id
-        params[:customerId]
-      end
-
-      def person_id
-        params[:personId]
-      end
-
-      def api_key
-        params[:api_key]
-      end
-    end
-
-    def initialize(connection_repo, params)
+    def initialize(connection_repo, mailer, params)
       @connection_repo = connection_repo
-      @params = Params.new(params)
+      @mailer = mailer
+      @params = params
     end
 
     def import
-      @candidate = Icims::Client.new(connection).candidate(params.person_id)
+      @candidate = Icims::Client.new(connection).candidate(person_id)
       @imported_result = namely_importer.single_import(candidate)
 
-      raise FailedImport.new unless imported_result.success?
+      if imported_result.success?
+        mailer.delay.successful_import(user, candidate)
+      else
+        mailer.delay.unsuccessful_import(user, candidate, imported_result)
+      end
+    rescue Icims::Client::Error => e
+      mailer.delay.unauthorized_import(user, e.message)
+    end
+
+    private
+
+    def person_id
+      params[:personId]
+    end
+
+    def customer_id
+      params[:customerId]
+    end
+
+    def api_key
+      params[:api_key]
     end
 
     def user
       connection.user
     end
 
-    private 
-
-    def connection 
-      @connection_repo.find_by(api_key: params.api_key, 
-                               customer_id: params.customer_id)
+    def connection
+      @connection_repo.find_by(api_key: api_key,
+                               customer_id: customer_id)
     end
 
     def namely_importer
@@ -45,6 +49,10 @@ module Icims
         namely_connection: user.namely_connection,
         attribute_mapper: Icims::AttributeMapper.new,
       )
+    end
+
+    def mailer
+      @mailer
     end
   end
 end
---
+++ spec/services/icims/candidate_importer_spec.rb
@@ -0,0 +1,56 @@
+require_relative '../../../app/services/icims/candidate_importer'
+
+describe Icims::CandidateImporter do 
+  subject(:service) { described_class.new(connection_repo, mailer, params) }
+  let(:connection_repo) { double :connection_repo, find_by: connection }
+  let(:connection) { double :connection, user: user }
+  let(:user) { double :user, namely_connection: namely_conn }
+  let(:mailer) { double :mailer, delay: delayed }
+  let(:namely_conn) { double :namely_conn, profiles: namely_profiles } 
+  let(:params) { {} }
+
+  describe '#import' do 
+    let(:delayed) { double :delayed }
+    let(:candidate) { double :candidate, 
+                      id: -1,
+                      firstname: 'Bob', 
+                      lastname: 'Burgers',
+                      email: 'example@email.com',
+                      start_date: 'start_date',
+                      gender: 'Alien',
+                      home_address: 'Et'}
+
+    context 'when importing successfully' do 
+      let(:namely_profiles) { double :profiles, create!: true }
+      it 'enqueue a successful mail delivery' do 
+        allow_any_instance_of(Icims::Client).to receive(:candidate) { candidate }
+        expect(delayed).to receive(:successful_import).with(user, 
+                                                            candidate)
+        service.import
+      end
+    end
+
+    context 'when unsucessful import' do 
+      let(:namely_profiles) { double :profiles }
+      it 'enqueue an unsuccessful mail delivery' do 
+        allow_any_instance_of(Icims::Client).to receive(:candidate) { candidate }
+        allow(namely_profiles).
+          to(receive(:create!) { raise Namely::FailedRequestError.new })
+
+        expect(delayed).to receive(:unsuccessful_import)
+        service.import
+      end
+    end
+
+    context 'when unauthorized credentials in icims' do 
+      let(:namely_profiles) { double :profiles }
+      it 'enqueue an unauthorized email' do 
+        allow_any_instance_of(Icims::Client).
+          to(receive(:candidate) { raise Icims::Client::Error.new 'Unauthorized'})
+
+        expect(delayed).to receive(:unauthorized_import).with(user, 'Unauthorized')
+        service.import
+      end
+    end
+  end
+end
