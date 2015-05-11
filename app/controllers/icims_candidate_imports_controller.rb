ec66221491b77dc7262c547a7b4ebf6711f2e541
cored <george.rafael@gmail.com>
2015-05-11 15:04:12 -0400

    Add initialization of connection for the current request

--- app/controllers/icims_candidate_imports_controller.rb
+++ app/controllers/icims_candidate_imports_controller.rb
@@ -3,9 +3,16 @@ class IcimsCandidateImportsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    Icims::CandidateImporter.new(Icims::Connection, 
-                                 IcimsCandidateImportMailer, 
+    Icims::CandidateImporter.new(connection,
+                                 IcimsCandidateImportMailer,
                                  params).import
     render nothing: true
+  end
+
+  private
+
+  def connection
+    Icims::Connection.find_by(api_key: params[:api_key],
+                              customer_id: params[:customerId])
   end
 end
