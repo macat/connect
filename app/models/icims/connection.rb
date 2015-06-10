5c005842860032001d53ee297bb011f8008d8267
cored <george.rafael@gmail.com>
2015-06-10 17:58:35 -0400

    Remove duplication from Icims::Connection#missing_namely_field?

--- app/models/icims/connection.rb
+++ app/models/icims/connection.rb
@@ -23,7 +23,7 @@ module Icims
 
     def missing_namely_field?
       if connected?
-        check_namely_field
+        UserCheckNamelyField.new(self).check?
       end
     end
 
@@ -33,27 +33,6 @@ module Icims
 
     def set_api_key
       self.api_key = SecureRandom.hex(20)
-    end
-
-    private
-
-    delegate :namely_connection, to: :user
-
-    def check_namely_field
-      if namely_field_not_found? && namely_account_has_required_field?
-        update(found_namely_field: true)
-      end
-      namely_field_not_found?
-    end
-
-    def namely_field_not_found?
-      !found_namely_field?
-    end
-
-    def namely_account_has_required_field?
-      namely_connection.fields.all.detect do |field|
-        field.name == required_namely_field
-      end
     end
   end
 end
