f3969a4f86a4f49b1afc47f3ce4c5243f712001b
cored <george.rafael@gmail.com>
2015-06-10 17:03:14 -0400

    Add index for secret_key and name for greenhouse_connection

--- db/migrate/20150427183558_create_greenhouse_connections.rb
+++ db/migrate/20150427183558_create_greenhouse_connections.rb
@@ -3,8 +3,8 @@ class CreateGreenhouseConnections < ActiveRecord::Migration
     create_table :greenhouse_connections do |t|
       t.timestamps null: false
 
-      t.string :secret_key
-      t.string :name
+      t.string :secret_key, index: true
+      t.string :name, index: true
       t.boolean :found_namely_field, null: false, default: false
       t.belongs_to :user, null: false, index: true
     end
