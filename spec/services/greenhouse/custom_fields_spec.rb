371858929fa0e412ecc2bcdf8fffc3c67bf60ed2
Attila Maczak <attila@maczak.hu>
2015-06-22 22:28:07 -0400

    split test

--- spec/services/greenhouse/custom_fields_spec.rb
+++ spec/services/greenhouse/custom_fields_spec.rb
@@ -9,21 +9,29 @@ describe Greenhouse::CustomFields do
       Namely::Model.new(nil, object)
     end
   end
-  let(:payload) do
-    {
-      "name_middle" => { "value" => "test",
-                         "type" => "short_text",
-                         "name" => "Middle name" },
-      "level" => { "value" => "ok",
-                   "type" => "long_text",
-                   "name" => "Job title" },
-      "notsupported" => { "value" => "", "type" => "notsupported" },
-    }
-  end
+
 
   describe "::match" do
+    let(:notsupported) do
+      {
+        "notsupported" => { "value" => "", "type" => "notsupported" },
+      }
+    end
     it "filters out not supported fields" do
-      expect(described_class.match(payload, namely_fields)).to eql(
+      expect(described_class.match(notsupported, namely_fields)).to eql({})
+    end
+    let(:payload_with_fields) do
+      {
+        "name_middle" => { "value" => "test",
+                           "type" => "short_text",
+                           "name" => "Middle name" },
+        "level" => { "value" => "ok",
+                     "type" => "long_text",
+                     "name" => "Job title" },
+      }
+    end
+    it "matches namely field name using title" do
+      expect(described_class.match(payload_with_fields, namely_fields)).to eql(
         "middle_name" => "test",
         "job_title" => "ok"
       )
