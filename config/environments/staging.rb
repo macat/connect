195d5d3e5a02b18f5bdfba463576709dbb1fa252
Derek Prior <derekprior@gmail.com>
2015-08-27 14:59:14 -0400

    Clean up configuration code
    * Remove unreferenced smtp.rb file
    * Remove explicit delayed job enabling - this is default.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -3,9 +3,9 @@ require_relative "production"
 Mail.register_interceptor(
   RecipientInterceptor.new(ENV.fetch("EMAIL_RECIPIENTS"))
 )
-Delayed::Worker.delay_jobs = true
-Rails.application.configure do
-  # ...
 
-  config.action_mailer.default_url_options = { host: 'staging.connect.com' }
+Rails.application.configure do
+  config.action_mailer.default_url_options = {
+    host: "namely-connect-staging.herokuapp.com"
+  }
 end
