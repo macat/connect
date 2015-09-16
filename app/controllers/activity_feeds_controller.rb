22ea34d29fbf1b8f5d1bf971a6d7b656ac1150f6
Derek Prior <derekprior@gmail.com>
2015-09-15 20:55:38 -0400

    Page Activity Feed
    Users will have at least one sync summary per day, more if they manually
    export. The first page of the user's most recent 10 activities should be
    sufficient. There was a desire to have the other activities available
    via paging.

--- app/controllers/activity_feeds_controller.rb
+++ app/controllers/activity_feeds_controller.rb
@@ -3,6 +3,8 @@ class ActivityFeedsController < IntegrationController
     @sync_summaries = connection.
       sync_summaries.
       includes(:successful_profile_events, :failed_profile_events).
-      ordered
+      ordered.
+      page(params[:page]).
+      per(10)
   end
 end
