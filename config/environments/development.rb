366bbc246d0968944e1311ef4a57400b3432e064
Attila Maczak <attila@maczak.hu>
2015-10-09 15:42:48 -0400

    add trusted_ip - needed for docker setup + better_errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,4 +39,6 @@ Rails.application.configure do
   config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { host: 'localhost:9000' }
+
+  BetterErrors::Middleware.allow_ip! ENV['TRUSTED_IP'] if ENV['TRUSTED_IP']
 end
