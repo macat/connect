d3e08fbff91416b7031b1bc6a70752409df142f6
Attila Maczak <attila@maczak.hu>
2015-11-10 10:24:08 -0500

    better fixtures for matcher spec

--- spec/services/net_suite/matcher_spec.rb
+++ spec/services/net_suite/matcher_spec.rb
@@ -1,55 +1,6 @@
 require "rails_helper"
 
 describe NetSuite::Matcher do
-  let(:employee1) do
-    {
-      "lastName" => "Wonderland",
-      "firstName" => "Alex",
-      "email" => "alex@example.com",
-      "internalId" => "TEST",
-    }
-  end
-  let(:employee2) do
-    {
-      "lastName" => "Wonderland",
-      "firstName" => "Alice",
-      "email" => "alice@example.com",
-      "internalId" => "TEST2",
-    }
-  end
-  let(:employee3) do
-    {
-      "lastName" => "Wonderland",
-      "firstName" => "None",
-      "email" => "none@example.com",
-      "internalId" => "TEST3",
-    }
-  end
-  let(:profile1) do
-    build(:namely_profile, {
-      "last_name" => "Wonderlandee",
-      "first_name" => "Alex",
-      "email" => "alex@example.com",
-      "netsuite_id" => "TEST",
-    })
-  end
-  let(:profile2) do
-    build(:namely_profile, {
-      "last_name" => "Wonderland",
-      "first_name" => "Alice",
-      "email" => "test@example.com",
-      "netsuite_id" => "TEST2",
-    })
-  end
-  let(:profile3) do
-    build(:namely_profile, {
-      "first_name" => "None",
-      "last_name" => "Wonderland",
-      "email" => "alice@example.com",
-      "netsuite_id" => "",
-    })
-  end
-
   let(:attribute_mapper) { create(:attribute_mapper) }
 
   before do
@@ -69,6 +20,39 @@ describe NetSuite::Matcher do
 
   describe "#matched" do
     context "when netsuite_id matches" do
+      let(:employee1) do
+        {
+          "lastName" => "Wonderland",
+          "firstName" => "Alex",
+          "email" => "alex@example.com",
+          "internalId" => "TEST",
+        }
+      end
+      let(:employee2) do
+        {
+          "lastName" => "Wonderland",
+          "firstName" => "Alice",
+          "email" => "alice@example.com",
+          "internalId" => "TEST2",
+        }
+      end
+      let(:profile1) do
+        build(:namely_profile, {
+          "last_name" => "Wonderlandee",
+          "first_name" => "Alex",
+          "email" => "alex@example.com",
+          "netsuite_id" => "TEST",
+        })
+      end
+      let(:profile2) do
+        build(:namely_profile, {
+          "last_name" => "Wonderland",
+          "first_name" => "Alice",
+          "email" => "test@example.com",
+          "netsuite_id" => "TEST5",
+        })
+      end
+
       it "returns list of matched objects" do
         matcher = described_class.new(
           mapper: attribute_mapper,
@@ -90,7 +74,52 @@ describe NetSuite::Matcher do
       end
     end
     context "when netsuite_id doesn't match" do
-      it "uses the fields list to match objects" do
+      let(:employee1) do
+        {
+          "lastName" => "Wonderland",
+          "firstName" => "Alex",
+          "email" => "alex@example.com",
+          "internalId" => "TEST",
+        }
+      end
+      let(:employee2) do
+        {
+          "lastName" => "Wonderland",
+          "firstName" => "Alice",
+          "email" => "alice@example.com",
+          "internalId" => "TEST2",
+        }
+      end
+      let(:employee3) do
+        {
+          "lastName" => "Wonderland",
+          "firstName" => "None",
+          "email" => "none@example.com",
+          "internalId" => "TEST3",
+        }
+      end
+      let(:profile1) do
+        build(:namely_profile, {
+          "last_name" => "Wonderlandee",
+          "first_name" => "Alex",
+          "email" => "alex@example.com",
+        })
+      end
+      let(:profile2) do
+        build(:namely_profile, {
+          "last_name" => "Wonderland",
+          "first_name" => "Alice",
+          "email" => "test@example.com",
+        })
+      end
+      let(:profile3) do
+        build(:namely_profile, {
+          "first_name" => "None",
+          "last_name" => "Wonderland",
+          "email" => "alice@example.com",
+        })
+      end
+      it "uses one field to match objects" do
         matcher = described_class.new(
           mapper: attribute_mapper,
           fields: ["email"],
