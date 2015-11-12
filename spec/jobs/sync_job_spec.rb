ccb83b9afa91c6282717f6b7becd4b2d72bc9706
BobbyTables <robert@creativequeries.com>
2015-11-12 13:17:30 -0500

    Remove dead specs.

--- spec/jobs/sync_job_spec.rb
+++ spec/jobs/sync_job_spec.rb
@@ -9,39 +9,4 @@ describe SyncJob do
 
     expect(Notifier).to have_received(:execute).with(connection)
   end
-
-  context 'when the connection is lockable' do
-    let(:user) { create(:user) }
-    let(:installation) { user.installation }
-    let(:connection) { double(:connection, lockable?: true, sync: []) }
-
-    before do
-      allow(connection).to receive(:locked?).and_return(lock_state)
-    end
-
-    context 'and it is locked' do
-      let(:lock_state) { true }
-
-      it 'does not run a sync' do
-        allow(connection).to receive(:sync).and_return([])
-
-        SyncJob.perform_now(connection)
-
-        expect(connection).to_not have_received(:sync)
-      end
-    end
-
-    context 'and it is unlocked' do
-      let(:connection) { build_stubbed(:net_suite_connection, :ready, installation: installation) }
-      let(:lock_state) { false }
-
-      it 'runs a sync' do
-        allow(connection).to receive(:sync).and_return([])
-
-        SyncJob.perform_now(connection)
-
-        expect(connection).to have_received(:sync)
-      end
-    end
-  end
 end
